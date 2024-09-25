d1cd45e031f51c25156cab6eefcbba4c71d18842
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-09-25 08:42:26 -0500

    style: :lipstick: estilo creado para los detalles de la pelicula

---
+++ src/components/CinemaSelection.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from '../styles/CinemaSelection.module.css'
+import movieTest from '../storage/img/logo1.png'
+import star from '../storage/img/star.png'
+
+export const CinemaSelection = () => {
+  return (
+    <div>
+        {/* componenete header */}
+        <div className={styles.container__image}>
+            <img src={movieTest} alt="" />
+        </div>
+        <div className={styles.detail__movie}>
+            <h1>name</h1>
+            <span>genero</span>
+            <p>sinopsis</p>
+            <div className={styles.container__ranking}>
+                <img src={star} alt="" />
+                <h3>ranking</h3>
+            </div>
+        </div>
+        <div className={styles.container__place}>
+            <h4>Cinema</h4>
+            <div className={styles.description__place}>
+                <h4>Campuslands</h4>
+                <span>Auditorium</span>
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+export default CinemaSelection
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import Home from "../components/Home"
 import Homeapp from "../components/Homeapp"
 import LogIn from "../components/LogIn"
 import SignUp from "../components/SignUp"
+import CinemaSelection from "../components/CinemaSelection"
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Route path="/LogIn" element={<LogIn />} />
       <Route path="/SignUp" element={<SignUp />} />
       <Route path="/HomeApp" element={<Homeapp />} />
+      <Route path="/CinemaSelection" element={<CinemaSelection />} />
       <Route path="/*" element={<Navigate to="/" />}/>
     </Routes>  
   )
